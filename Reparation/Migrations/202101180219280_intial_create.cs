17e3c68bae9a26735b3d211bc9fa1596f53ccf57
meRahulSingh <mr.rahulsingh@hotmail.com>
2021-01-18 03:22:38 +0100

    Changing azure subscriptio

---
+++ Reparation/Migrations/202101180219280_intial_create.cs
@@ -0,0 +1,16 @@
+﻿namespace Reparation.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class intial_create : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
